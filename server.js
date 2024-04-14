83dccc47c67697cb157d93bd12dc1063fff827c3
Hezekiahs <ok.hezekiahs@gmail.com>
2024-04-14 16:52:44 +0100

    edit env

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const methodOverride = require("method-override");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "volt_meme",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 initializePassport(passport);
@@ -182,4 +182,4 @@ function checkNotAuthenticated(req, res, next) {
   next();
 }
 
-app.listen(3000);
+app.listen(process.env.PORT);
